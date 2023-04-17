b20c2719f7ba445c2c5f3f881009a37e9452f945
alfredoisg <alfredo.sanchez@sintef.no>
2023-04-17 15:53:07 +0200

    decided to put in same file, since it inherits from triplestore and shares many imported packages

--- tripper/triplestore.py
+++ tripper/triplestore.py
@@ -1005,8 +1005,8 @@ class Triplestore:
             standard=standard,
             cost=cost,
         )
-
-## ==============================================================================
+    
+class TriplestoreDataHandler(Triplestore):
 
     def add_data(
         self,
