4e9566ca877ac57a794c1ece0d18a42aae048c72
alfredoisg <alfredo.sanchez@sintef.no>
2023-05-31 10:20:16 +0200

    this should fix the CI-Tests: attempt 3

--- tripper/triplestore.py
+++ tripper/triplestore.py
@@ -1087,8 +1087,8 @@ class TriplestoreDataHandler(Triplestore):
             Assume we have a data source that relates water temperature
             (mapped to EX.Temp) to the amount of blue-green algae (mapped to
             EX.AlgaeConc). We can then register it with
-            >>> from triplestore import Triplestore as ts
-            >>> from triplestore import TriplestoreDataHandler as tsdh
+            >>> import triplestore.Triplestore as ts
+            >>> import triplestore.TriplestoreDataHandler as tsdh
 
             >>> temp = tsdh.add_data(...)  # Data source with temperatures
             >>> conc = tsdh.add_data(...)  # Data source with algae conc.
