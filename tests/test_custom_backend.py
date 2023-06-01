b856d8121514ec003e7457fdaa77660fb829e1a0
Jesper Friis <jesper.friis@sintef.no>
2023-06-01 21:38:54 +0200

    Remove duplicated test

--- tests/test_custom_backend.py
+++ tests/test_custom_backend.py
@@ -4,8 +4,3 @@ from tripper import Triplestore
 # Test relative import
 ts = Triplestore(backend="backends.dummy", package="backends")
 assert list(ts.triples()) == [("a", "b", "c"), ("d", "e", "f"), ("g", "h", "i")]
-
-
-# Test entry point
-ts2 = Triplestore(backend="backends.dummy", package="backends")
-assert list(ts.triples()) == [("a", "b", "c"), ("d", "e", "f"), ("g", "h", "i")]
