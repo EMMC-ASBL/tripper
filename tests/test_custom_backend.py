18acab3ac1fd5bf97ca060047e597480304d5935
alfredoisg <alfredo.sanchez@sintef.no>
2023-06-06 10:49:17 +0200

    fixing line too long pylint error

--- tests/test_custom_backend.py
+++ tests/test_custom_backend.py
@@ -3,4 +3,8 @@ from tripper import Triplestore
 
 # Test relative import
 ts = Triplestore(backend="backends.dummy", package="backends")
-assert list(ts.triples()) == [("a", "b", "c"), ("d", "e", "f"), ("g", "h", "i")]
+assert list(ts.triples()) == [
+    ("a", "b", "c"),
+    ("d", "e", "f"),
+    ("g", "h", "i"),
+]
