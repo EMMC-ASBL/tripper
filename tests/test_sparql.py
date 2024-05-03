f746f076142b24697db7bffea03fa0e004f338e6
Jesper Friis <jesper.friis@sintef.no>
2024-05-03 21:21:33 +0200

    Added sparql test.

--- tests/test_sparql.py
+++ tests/test_sparql.py
@@ -36,3 +36,52 @@ def test_sparql():
     r = ts.query(query)
     assert (EMMO.Metre, "m") in r
     assert (EMMO.AstronomicalUnit, "au") in r
+
+
+# if True:
+def test_sparql_construct():
+    """Test SPARQL construct query."""
+    from textwrap import dedent
+
+    from tripper import Triplestore
+
+    data = dedent(
+        """
+        @prefix  foaf:  <http://xmlns.com/foaf/0.1/> .
+
+        _:a foaf:givenname   "Alice" .
+        _:a foaf:family_name "Hacker" .
+        _:b foaf:firstname   "Bob" .
+        _:b foaf:surname     "Hacker" .
+        """
+    )
+    query = dedent(
+        """
+        PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+        PREFIX vcard:   <http://www.w3.org/2001/vcard-rdf/3.0#>
+
+        CONSTRUCT {
+          ?x  vcard:N _:v .
+          _:v vcard:givenName ?gname .
+          _:v vcard:familyName ?fname
+        }
+        WHERE {
+          { ?x foaf:firstname ?gname } UNION  { ?x foaf:givenname   ?gname } .
+          { ?x foaf:surname   ?fname } UNION  { ?x foaf:family_name ?fname } .
+        }
+        """
+    )
+    ts = Triplestore("rdflib")
+    ts.parse(data=data)
+    VCARD = ts.bind("vcard", "http://www.w3.org/2001/vcard-rdf/3.0#")
+
+    r = ts.query(query)
+
+    assert len(r) == 6
+    assert len([s for s, p, o in r if p == VCARD.givenName]) == 2
+    assert (
+        len([s for s, p, o in r if p == VCARD.givenName and o == "Bob"]) == 1
+    )
+    assert (
+        len([s for s, p, o in r if p == VCARD.givenName and o == "Cyril"]) == 0
+    )
