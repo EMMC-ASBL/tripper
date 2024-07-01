0c23395fc37f790160eb5a6d5490fa3f26cb305b
Jesper Friis <jesper.friis@sintef.no>
2024-07-01 17:19:50 +0200

    Cleaned up backend tests

--- tests/backends/test_rdflib.py
+++ tests/backends/test_rdflib.py
@@ -3,34 +3,32 @@
 Most of the rdflib backend is already tested in tests/test_triplestore.py.
 """
 
-import pytest
-
-from tripper import RDFS, Literal, Triplestore
-
-rdflib = pytest.importorskip("rdflib")
+# pylint: disable=invalid-name
 
 
-ts = Triplestore("rdflib")
-EX = ts.bind("", "http://ex#")
+def test_rdflib_backend():
+    """Test rdflib backend."""
+    from tripper import RDFS, Literal, Triplestore
 
+    ts = Triplestore("rdflib")
+    EX = ts.bind("", "http://ex#")
 
-# Test for issue #162: Literals are lost when accessing triples with rdflib
-ts.parse(
-    format="turtle",
-    data=(
-        "<http://ex#s> <http://ex#p> "
-        '"abc"^^<http://www.w3.org/2001/XMLSchema#string> .'
-    ),
-)
-assert list(ts.triples()) == [
-    (
-        "http://ex#s",
-        "http://ex#p",
-        Literal("abc", datatype="http://www.w3.org/2001/XMLSchema#string"),
+    # Test for issue #162: Literals are lost when accessing triples with rdflib
+    ts.parse(
+        format="turtle",
+        data=(
+            "<http://ex#s> <http://ex#p> "
+            '"abc"^^<http://www.w3.org/2001/XMLSchema#string> .'
+        ),
     )
-]
+    assert list(ts.triples()) == [
+        (
+            "http://ex#s",
+            "http://ex#p",
+            Literal("abc", datatype="http://www.w3.org/2001/XMLSchema#string"),
+        )
+    ]
 
-
-# Test for bnode
-ts.add_triples([("_:bn1", RDFS.subClassOf, EX.s)])
-assert ts.value(predicate=RDFS.subClassOf, object=EX.s) == "_:bn1"
+    # Test for bnode
+    ts.add_triples([("_:bn1", RDFS.subClassOf, EX.s)])
+    assert ts.value(predicate=RDFS.subClassOf, object=EX.s) == "_:bn1"
