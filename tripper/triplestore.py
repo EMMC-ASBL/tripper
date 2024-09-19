887f1412f6ed6c80c569843c466e250e984bc3ea
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2024-09-19 12:00:54 +0200

    Fix naming of blanknodes

--- tripper/triplestore.py
+++ tripper/triplestore.py
@@ -593,6 +593,7 @@ class Triplestore:
             None.
         """
         spo = (subject, predicate, object)
+        print('spo',spo)
         if sum(iri is None for iri in spo) != 1:
             raise ValueError(
                 "Exactly one of `subject`, `predicate` or `object` must be "
