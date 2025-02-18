c21cf775d9a9464aa84d7e71bcd7ed1b6bce6c6a
Francesca L. Bleken <48128015+francescalb@users.noreply.github.com>
2025-02-18 11:14:05 +0100

    Update tests/backends/test_sparqlwrapper_graphdb.py
    Co-authored-by: Jesper Friis <jesper-friis@users.noreply.github.com>

--- tests/backends/test_sparqlwrapper_graphdb.py
+++ tests/backends/test_sparqlwrapper_graphdb.py
@@ -1,7 +1,7 @@
 """
 Test the sparqlwrapper backend with GraphDB.
 Note that this requires to have a running graphDB instance set up
-as described in the for_developers documentation.
+as described in the for_developers documentation on https://emmc-asbl.github.io/tripper/latest/developers/.
 """
 
 import time
