33c1f5a2af726277785db316a1f14c3cdcc54124
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2025-11-03 11:08:51 +0100

    Added test that fail and some pritn statements that show error

--- tripper/datadoc/dataset.py
+++ tripper/datadoc/dataset.py
@@ -1213,7 +1213,7 @@ def make_query(
     context = get_context(keywords=keywords)
     context._create_caches()  # pylint: disable=protected-access
     expanded = context._expanded  # pylint: disable=protected-access
-
+    print("expanded", expanded)
     # Add prefixes to triplestore
     ts.namespaces.update(keywords.get_prefixes())
 
@@ -1253,6 +1253,8 @@ def make_query(
         """Add criteria to SPARQL query."""
         nonlocal n
         key = f"@{k[1:]}" if k.startswith("_") else k
+        print("key", key)
+        print("value", v)
         if isinstance(v, list):
             for ele in v:
                 add_crit(key, ele, regex=regex, s=s)
@@ -1305,6 +1307,10 @@ def make_query(
       {where_statements}
     }}
     """
+    print("---")
+    print(query)
+    print("---")
+
     return query
 
 
